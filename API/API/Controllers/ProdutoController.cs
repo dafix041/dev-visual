c5fe95a196de7be1d552c551b95510da065dfd73
dafix.041 <vcruzz1977@gmail.com>
2023-08-31 00:09:20 -0300

    implementando buscar, listar e cadastrar

--- API/API/Controllers/ProdutoController.cs
+++ API/API/Controllers/ProdutoController.cs
@@ -5,20 +5,43 @@ namespace API;
 [Route("api/produto")]
 public class ProdutoController : ControllerBase
 {
-    private List<Produto> produtos = new List<Produto>();
+    private static List<Produto> produtos = new List<Produto>();
+
+    
     //GET: api/produto/listar
     [HttpGet]
     [Route("listar")]
-    public string Listar()
-    {
-        return "Hello World de uma API em C# com Watch";
-    }
+    public IActionResult Listar() =>
 
+     produtos.Count == 0 ? NotFound() : Ok(produtos);
+
+    //GET: api/produto/buscar/bolacha{}
+    [HttpGet]
+    [Route("buscar/{nome}")]
+    public IActionResult Buscar([FromRoute] string nome)
+    {
+        foreach (Produto produtoCadastrado in produtos)
+        {
+            if (produtoCadastrado.Nome == nome)
+            {
+                return Ok(produtoCadastrado);
+            }
+        }
+        return NotFound();
+    }
+    //POST: api/produto/cadastrar
     [HttpPost]
     [Route("cadastrar")]
-    public Produto Cadastrar(Produto produto)
+    public IActionResult Cadastrar([FromBody] Produto produto)
     {
-        return produto;
+        produtos.Add(produto);
+        return Created("", produto);
     }
 
 }
+
+// if(produtos.Count == 0){
+
+//     return NotFound();
+// }
+// return Ok(produtos);
